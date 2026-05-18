59d6deee0400c37d7c92bd2cb53ec134ea9f9d5e
Jonas Wagner <jwagner@knoppiks.de>
2026-05-18 12:11:39 +0200

    Add Missing TC Agent Role

--- tc-agent.tf
+++ tc-agent.tf
@@ -10,3 +10,9 @@ resource "keycloak_openid_client" "fts_agent" {
   access_type              = "CONFIDENTIAL"
   service_accounts_enabled = true
 }
+
+resource "keycloak_openid_client_service_account_realm_role" "fts_agent_ttp_admin" {
+  realm_id                = data.keycloak_realm.distance_xt.id
+  service_account_user_id = keycloak_openid_client.fts_agent.service_account_user_id
+  role                    = keycloak_role.ttp_admin_all.name
+}
